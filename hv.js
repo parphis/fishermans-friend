2d4f3c66524aa92ced35cea57a87c4f0e192d211
istvan vig <istvan.vig@parphis.hu>
2017-01-25 15:22:40 +0100

    Quiz implementation

--- hv.js
+++ hv.js
@@ -1,7 +1,12 @@
+quizTypes = {
+	0: "choices",
+	1: "freetext"
+};
 function FishermansFriend() {
 	// data of the fishes
 	this._fishes = _fishlist;
-	this._countOfFishes = Object.keys(this._fishes).length;
+	this._countOfFishes = Object.keys(_fishlist).length;
+	this._categoryCount = Object.keys(cts).length;
 	this._fishImageExt = ".jpg";
 	this._fishImageFolder = "kepek/";
 	this._fishImage = "";
@@ -40,55 +45,26 @@ function FishermansFriend() {
 	this._learnEl = undefined;
 	this._answersEl = undefined;
 	this._quizImageEl = undefined;
+	this._nextQuestionEl = undefined;
 
 	// quiz
 	this._whatToTest = "";
 	this._quizData = Array();
+	this._quizType;
+	this._oneFish; // one fish item
 	this._possibleAnswers = 3;
+	this._rightAnswer;
 	this._btnDefaultClass = "w3-btn w3-tiny w3-white w3-border w3-border-blue";
 	this._btnRightClass = "w3-btn w3-tiny w3-green w3-border w3-border-blue";
 	this._btnFalseClass = "w3-btn w3-tiny w3-red w3-border w3-border-blue";
+
+	var self;
 }
 
 FishermansFriend.prototype = {
 	constructor: FishermansFriend,
 
-	_randomize: function(max) {
-		return Math.floor(Math.random()*max);
-	},
-
-	_orderByCategory: function () {
-		var tmp = Array();
-		var c;
-		var s;
-		var f;
-		for(i=0; i<this._countOfFishes; i++) {
-			c = this._fishes[i]["category"];
-			s = Object.keys(tmp).length;
-			if(s==0) {
-				tmp.push(this._fishes[i]);
-			}
-			else {
-				f = false;
-				for(j=0; j<s; j++) {
-					if(tmp[j]["category"]==c) {
-						tmp.splice(j, 0, this._fishes[i]);
-						f = true;
-						break;
-					}	
-				}
-				if(f==false) {
-					tmp.push(this._fishes[i]);
-				}
-			}
-		}
-		this._fishes = tmp;
-	},
-
-	_orderByName: function() {
-		this._fishes = _fishlist;
-	},
-
+	// functions called from the page begin with _
 	_getElements: function () {
 		if (typeof this._mainmenuEl === 'undefined') {
 			this._displayEl = document.getElementById("details");
@@ -100,6 +76,7 @@ FishermansFriend.prototype = {
 			this._detailsEl = document.getElementById("details");
 			this._answersEl = document.getElementById("answers");
 			this._quizImageEl = document.getElementById("quizImage");
+			this._nextQuestionEl = document.getElementById("nextQuestion");
 		}
 	},
 
@@ -129,17 +106,17 @@ FishermansFriend.prototype = {
 			this._mainmenuEl.style.display = 'none';
 		}
 		if(ordering=="byName") {
-			this._orderByName();
+			this.orderByName();
 		}
 		if(ordering=="byCategory") {
-			this._orderByCategory();
+			this.orderByCategory();
 		}
 		this._c = 0;
 		this.displayFishImage();
 		this.displayFishData();
 	},
 	// hide 'learn' div and show 'quiz' div
-	_learn: function (what) {
+	_learn: function (what, type) {
 	
 		if(typeof this._quizEl !== 'undefined') {
 			this._quizEl.style.display = 'block';
@@ -152,7 +129,79 @@ FishermansFriend.prototype = {
 		}
 		
 		this._whatToTest = what;
-		this._testIt();
+		this._quizType = type;
+		this.getRandomElement();
+	},
+
+	_prev: function () {
+		this._c--;
+
+		if (this._c < 0) {
+			this._c = this._countOfFishes-1;
+		}
+
+		this.displayFishImage();
+		this.displayFishData();
+	},
+
+	_next: function () {
+		this._c++;
+
+		if (this._c >= this._countOfFishes) {
+			this._c = 0;
+		}
+
+		this.displayFishImage();
+		this.displayFishData();
+	},
+
+	randomize: function (max) {
+		return Math.floor(Math.random()*max);
+	},
+
+	getRandomElement: function () {
+		var rnd = this.randomize(this._countOfFishes);
+		self = this;
+		self._quizData.length = 0;
+		self._answersEl.innerHTML = "";
+		self._nextQuestionEl.style.display = "none";
+		// this will be the right answer
+		self._oneFish = this._fishes[rnd];
+		// get random item's image file name and show it
+		this.generateImageFileName(this._fishes[rnd]["hu_name"]);
+		this.displayQuizImage();
+	},
+
+	orderByCategory: function () {
+		var tmp = Array();
+		var c;
+		var s;
+		var f;
+		for(i=0; i<this._countOfFishes; i++) {
+			c = this._fishes[i]["category"];
+			s = Object.keys(tmp).length;
+			if(s==0) {
+				tmp.push(this._fishes[i]);
+			}
+			else {
+				f = false;
+				for(j=0; j<s; j++) {
+					if(tmp[j]["category"]==c) {
+						tmp.splice(j, 0, this._fishes[i]);
+						f = true;
+						break;
+					}	
+				}
+				if(f==false) {
+					tmp.push(this._fishes[i]);
+				}
+			}
+		}
+		this._fishes = tmp;
+	},
+
+	orderByName: function() {
+		this._fishes = _fishlist;
 	},
 
 	// based on https://gist.github.com/alisterlf/3490957 [BlackCode7]
@@ -209,92 +258,127 @@ FishermansFriend.prototype = {
 		}
 	},
 
-	displayQuizImage: function (idx) {
-		this._quizImageEl.src = this._quizData[idx]["img"];
+	displayQuizImage: function () {
+		self._quizImageEl.src = this._fishImage;
+		self._quizImageEl.addEventListener("click", self.startTest);
 	},
 
 	displayQuizData: function () {
-		if (typeof this._answersEl !== 'undefined') {
-			for(i=0; i<this._quizData.length; i++) {
+		// choices tests
+		if(self._quizType==quizTypes[0]) {
+			var limit = 0;
+			if(self._whatToTest=="hu_name") {
+				// reorder the possible answers array randomly
+				var rnd = self.randomize(self._possibleAnswers);
+				var tmp = "";
+				limit = self._possibleAnswers;
+
+				self._rightAnswer = rnd;
+				tmp = self._quizData[rnd];
+				self._quizData[rnd] = self._quizData[0];
+				self._quizData[0] = tmp;
+			}
+			if(self._whatToTest=="category") {
+				limit = self._categoryCount;
+				self._rightAnswer = self._oneFish["category"];
+			}
+			
+			for(i=0; i<limit; i++) {
 				var btn = document.createElement("span");
-				btn.appendChild(document.createTextNode(this._quizData[i]["data"]));
-				btn.className = this._btnDefaultClass;
+				btn.appendChild(document.createTextNode(self._quizData[i]));
+				btn.className = self._btnDefaultClass;
+				btn.style.clear = "both";
+				btn.style.float = "left";
 				btn.id = i;
-				// use .bind(this) here to be able to access this object's this._rightAnswer
-				// member variable from within the onclick callback
-				btn.addEventListener("click", this._checkIt.bind(this));
-				this._answersEl.appendChild(btn);
+				btn.addEventListener("click", self.checkIt);
+				self._answersEl.appendChild(btn);
 			}
 		}
-	},
-
-	_prev: function () {
-		this._c--;
-
-		if (this._c < 0) {
-			this._c = this._countOfFishes-1;
+		// free text typing tests
+		if(self._quizType==quizTypes[1]) {
+			var inp = document.createElement("input");
+			inp.className = "w3-input w3-border";
+			inp.type = "text";
+			inp.id = "freetext";
+			inp.addEventListener("keyup", self.checkIt);
+			self._answersEl.appendChild(inp);
 		}
-
-		this.displayFishImage();
-		this.displayFishData();
 	},
 
-	_next: function () {
-		this._c++;
+	startTest: function () {
+		// choices tests
+		if(self._quizType==quizTypes[0]) {
+			// guess the name of the displayed fish from three available answers
+			if(self._whatToTest=="hu_name") {
+				var i = 0;
+				// avoid infinite loop when checking the random names for multiplications
+				var tries = 0;
+				var max_tries = 5;
 
-		if (this._c >= this._countOfFishes) {
-			this._c = 0;
-		}
-
-		this.displayFishImage();
-		this.displayFishData();
-	},
-
-	_testIt: function () {
-		var rnd ;
-
-		this._quizData.length = 0;
-		this._answersEl.innerHTML = "";
-
-		// get random items from _fishes
-		for(i=0; i<this._possibleAnswers; i++) {
-			rnd = this._randomize(this._countOfFishes);
-
-			this.generateImageFileName(this._fishes[rnd]["hu_name"]);
-
-			var d = this._whatToTest.split(":");
-			var str = "";
-
-			if(d.length==1) {
-				str = this._fishes[rnd][d[0]];
-			}
-			else {
-				for(j=0; j<d.length; j++) {
-					str += this._fishes[rnd][d[j]];
-					if(j<d.length-1) {
-						str += " - ";
+				self._quizData[i] = self._oneFish["hu_name"];
+				for(i=1; (i<self._possibleAnswers) && (tries<max_tries); i++) {
+					var tmp = self._fishes[self.randomize(self._countOfFishes)]["hu_name"];
+					if(self._quizData.indexOf(tmp)>-1) {
+						i--;
+						tries++;
+					}
+					else {
+						self._quizData[i] = tmp;
 					}
 				}
+				self.displayQuizData();
 			}
-			this._quizData[i] = {"img": this._fishImage, "data": str};
+			if(self._whatToTest=="category") {
+				for(i=0; (i<self._categoryCount); i++) {
+					self._quizData[i] = cts[i];
+				}
+				self.displayQuizData();
+			}
 		}
-		// again new random item but now from the 3 collected above
-		rnd = this._randomize(this._possibleAnswers);
-		this._rightAnswer = rnd;
-		this.displayQuizImage(rnd);
-		this.displayQuizData();
-		console.log(this._rightAnswer);
+		// free text typing tests
+		if(self._quizType==quizTypes[1]) {
+			if(self._whatToTest=="hu_name") {
+				self._rightAnswer = self._oneFish["hu_name"];
+			}
+			self.displayQuizData();
+		}
+
+		// remove the click event handler from the fish image first
+		self._quizImageEl.removeEventListener("click", self.startTest);
 	},
 
-	_checkIt: function (evt) {
+	checkIt: function (evt) {
 		var el = evt.target;
-		// user clicked on the right answer
-		if(el.id==this._rightAnswer) {
-			el.className = this._btnRightClass;
+		if(self._quizType==quizTypes[0]) {
+			// user clicked on the right answer
+			if(el.id==self._rightAnswer) {
+				el.className = self._btnRightClass;
+				self._nextQuestionEl.innerHTML = "Következő kérdés";
+			}
+			else {
+				el.className = self._btnFalseClass;
+				document.getElementById(self._rightAnswer).className = self._btnRightClass;
+				if(self._whatToTest=="hu_name") {
+					self._nextQuestionEl.innerHTML = "A hal neve: "+self._oneFish["hu_name"];
+				}
+				else if(self._whatToTest=="category") {
+					self._nextQuestionEl.innerHTML = self._oneFish["hu_name"]+": "+cts[self._oneFish["category"]];
+				}
+			}
+			self._nextQuestionEl.style.display = "block";
 		}
-		else {
-			el.className = this._btnFalseClass;
-			document.getElementById(this._rightAnswer).className = this._btnRightClass;
+		if(self._quizType==quizTypes[1]) {
+			if(evt.keyCode==13) {
+				if(el.value.toUpperCase()==self._rightAnswer) {
+					el.style.backgroundColor = "green";
+					self._nextQuestionEl.innerHTML = "Következő kérdés";
+				}
+				else {
+					el.style.backgroundColor = "red";
+					self._nextQuestionEl.innerHTML = "A hal neve: "+self._oneFish["hu_name"];
+				}
+				self._nextQuestionEl.style.display = "block";
+			}
 		}
 	}
 };
